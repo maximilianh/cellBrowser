5bddf209d0ac1993827c827e503047782212b19c
Maximilian Haeussler <maximilianh@gmail.com>
2018-10-11 20:07:50 +0200

    pipifying everything

---
+++ pypi/README.md
@@ -0,0 +1,6 @@
+This is a package with a script that creates a web-based viewer for single cell
+data. The main script is cbBuild. It accepts an expression matrix and cell
+annotation (meta data) file. It creates an index.html, various .json and binary
+data files, copies javascript files into the same directory. The resulting
+webpage allows to move between datasets and to color the plot by gene or
+annotation.
---
+++ pypi/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cellbrowser",
+    version="0.4",
+    python_requires='>=2.5',
+    author="Maximilian Haeussler",
+    author_email="max@soe.ucsc.edu",
+    url="https://github.com/maximilianh/cellBrowser",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("../src/cbPyLib/"),
+    package_dir={'':'../src/cbPyLib/'},   # tell distutils packages are under src
+    include_package_data=True,  # use MANIFEST.in for non-python files
+    #package_data={
+        #'cellbrowser': ['cbWeb/js/*.js', 'cbWeb/html/*', 'cbWeb/ext/*']
+    #},
+    entry_points={
+    'console_scripts': [
+        'cbBuild = cellbrowser:cbBuildCli',
+        'cbScanpy = cellbrowser:cbScanpyCli',
+        'cbTool = cellbrowser:cbToolCli',
+        'cbUpgrade = cellbrowser:cbMake_cli'
+    ]
+    },
+    #package_data={
+        # If any package contains *.txt files, include them:
+        #'': ['*.txt'],
+        # And include any *.dat files found in the 'data' subdirectory
+        # of the 'mypkg' package, also:
+        #'mypkg': ['data/*.dat'],
+    #}
+
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript"
+    ],
+)
