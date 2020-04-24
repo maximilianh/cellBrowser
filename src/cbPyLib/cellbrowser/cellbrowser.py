d5df4b113334a8bc57c2fb0b825f6f8f30628f3d
Max <max@soe.ucsc.edu>
2020-04-24 13:11:04 -0700

    fixing stupid bug due to sloppy commit, refs #25329

--- src/cbPyLib/cellbrowser/cellbrowser.py
+++ src/cbPyLib/cellbrowser/cellbrowser.py
@@ -2412,7 +2412,7 @@ def nonAlphaToUnderscores(name):
     " for tab-sep tables: replace nonalpha chars with  underscores "
     assert(name!=None)
     #newName = to_camel_case(name.replace(" ", "_"))
-    newName = newName.replace("+", "Plus").replace("-", "Minus").replace("%", "Perc")
+    newName = name.replace("+", "Plus").replace("-", "Minus").replace("%", "Perc")
     newName = re.sub("[^a-zA-Z0-9_]","_", newName)
     newName = re.sub("^_","", newName)  # remove _ prefix
     logging.debug("Sanitizing %s -> %s" % (repr(name), newName))
