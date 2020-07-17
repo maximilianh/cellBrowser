3433f466a893c95243bf03f3b3ca166f871e75b8
Max <max@soe.ucsc.edu>
2020-07-17 05:54:59 -0700

    adding first minimal test, refs #24692

---
+++ test/runTests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+if [ ! -f pbmc_small/exprMatrix.tsv.gz ]; then
+        mkdir pbmc_small
+        wget https://cells-test.gi.ucsc.edu/downloads/test/pbmc_small/exprMatrix.tsv.gz -O pbmc_small/exprMatrix.tsv.gz
+fi
+../src/cbScanpy -e pbmc_small/exprMatrix.tsv.gz -o scanpyout -n pbmcSmall 
+#../src/cbImportScanpy -i scanpyout/anndata.h5ad -o importScanpyOut
+../src/cbSeurat -e exprMatrix.tsv.gz -o seuratOut -n pbmcSmallSeurat
+#../src/cbImportSeurat -i seurat2.rds -o cbImportSeurat
+#cd 5kPbmcMtx/ && cbTool mtx2tsv matrix.mtx.gz barcodes.tsv.gz features.tsv.gz exprMatrix.tsv.gz && cd ..
