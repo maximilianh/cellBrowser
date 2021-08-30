1b22a8337c5799ff36fa69bf37310cf6da495a62
Brian Raney <braney@soe.ucsc.edu>
2021-08-30 14:59:40 -0700

    some work on the CB release process

---
+++ build/doGitReports.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -ex
+
+directory=/hive/groups/qa/CB-git-reports-history/$CBVERSION
+
+if test -d $directory
+then
+    echo "Reports have already been done for $CBVERSION.  If you want to re-do them then:"
+    echo "rm -rf /hive/groups/qa/CB-git-reports-history/$CBVERSION"
+    exit 1
+fi
+
+mkdir  $directory
+indexFile=$directory/index.html
+
+echo "<html><head><title>git-reports</title></head><body>" > $indexFile
+echo "<h1>GIT changes: cell browser</h1>" >> $indexFile
+echo "<ul>" >> $indexFile
+echo "<li><a href=review/index.html>review</a> ($CBLASTDATE to $CBDATE)" >> $indexFile
+echo "<li><a href=/CB-git-reports-history/>Previous versions</a>" >> $indexFile
+echo "</body></html>" >> $indexFile
+
+#mv /hive/groups/qa/CB-git-reports/* /hive/groups/qa/CB-git-reports-history/$CBLASTVERSION
+#git log --tags --simplify-by-decoration --pretty="format:%ai %d"
+
+git-reports $CBLASTVERSION HEAD $CBLASTDATE $CBDATE review .. $directory review
+
+rm -rf /hive/groups/qa/CB-git-reports
+cp -r $directory /hive/groups/qa/CB-git-reports
+
+#cp /hive/groups/qa/CB-git-reports-history/v0.7.9/index.html /hive/groups/qa/CB-git-reports
--- ucsc/doGitReports.sh
+++
@@ -1,8 +0,0 @@
-mkdir /hive/groups/qa/CB-git-reports-history/v0.7.9
-mv /hive/groups/qa/CB-git-reports/* /hive/groups/qa/CB-git-reports-history/v0.7.9
-git log --tags --simplify-by-decoration --pretty="format:%ai %d"
-
-
-git-reports v0.7.9 HEAD 2020-04-20 2020-06-12 review `pwd` /hive/groups/qa/CB-git-reports review
-
-cp /hive/groups/qa/CB-git-reports-history/v0.7.9/index.html /hive/groups/qa/CB-git-reports
